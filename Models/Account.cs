49b490e0ba3e8564180bf432146f3e919662db22
Simón Holmquist <simon.holmquist@gmail.com>
2020-06-23 18:05:49 -0300

    fuck scaffolding

--- Models/Account.cs
+++ Models/Account.cs
@@ -5,10 +5,11 @@ namespace HayEquipo.Models
     public class Account
     {
         private int id;
-        private int mail;
+        private string mail;
         private string password;
 
         public string Mail { get => mail; set => mail = value; }
         public string Password { get => password; set => password = value; }
+        public int Id { get => id; set => id = value; }
     }
 }
